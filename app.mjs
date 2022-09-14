39badd97b6ae13bfa55c1b4ecbc78107b155a077
kotobukid <kotobuki@daqua.jp>
2022-09-14 15:19:16 +0900

    POST /user をSwaggerドキュメントに基づき実装した

--- app.mjs
+++ app.mjs
@@ -1,12 +1,17 @@
 import express from 'express';
 import {allowCrossDomain} from "./middlewares/custom-header.mjs";
 import {log_path} from "./middlewares/log-path.mjs";
+import bodyParser from 'body-parser'
 
 const app = express();
 const port = 3000;
 
 app.use(allowCrossDomain)
 app.use(log_path)
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(bodyParser.json())
 
 app.get('/users', (req, res) => {
     res.json([
@@ -21,6 +26,10 @@ app.get('/users', (req, res) => {
     );
 });
 
+app.post('/users', (req, res) => {
+    console.log(req.body)
+    res.status(201).send('CREATED');
+});
 
 app.get('/', (req, res) => {
     res.send('Hello world')
